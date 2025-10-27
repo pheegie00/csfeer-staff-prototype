5b099b88c37166b802024be63c332d10104b7365
Mohammad Taleb <t@lebbits.com>
2025-10-27 13:16:03 -0400

    feat: add OIDC user role extension and update configurations

--- csfeer/urls.py
+++ csfeer/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 """
 
 from django.apps import apps
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path("", TemplateView.as_view(template_name="index.html"), name="index"),
     path("admin/", admin.site.urls),
     path("oidc/", include("oauth2_authcodeflow.urls")),
 ]
@@ -28,3 +32,6 @@ if apps.is_installed("pattern_library"):
     urlpatterns += [
         path("pattern-library/", include("pattern_library.urls")),
     ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
