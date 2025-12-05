f6ef1e5f0b0596b19afaf8256b8bd1007001f1ed
Ryan Bagwell <ryan@ryanbagwell.com>
2025-12-05 12:17:47 -0500

    Fixes a problem with form rendering due to the annotation patterns (#99)

--- form_manager/schema/forms/tribal_short_form.py
+++ form_manager/schema/forms/tribal_short_form.py
@@ -177,24 +177,22 @@ class TribalShortFormFields(BaseFormFields):
         ),
     ]
 
-    total_expenditures: Optional[
-        Annotated[
-            CalculatedCurrencyField,
-            Field(
-                json_schema_extra={
-                    "fields": [
-                        "asset_building_expenditure",
-                        "housing_expenditure",
-                        "health_expenditure",
-                        "civic_expenditure",
-                        "transportation_expenditure",
-                        "partnerships_expenditure",
-                        "other_expenditure",
-                    ]
-                }
-            ),
-        ]
-    ]
+    total_expenditures: Annotated[
+        CalculatedCurrencyField,
+        Field(
+            json_schema_extra={
+                "fields": [
+                    "asset_building_expenditure",
+                    "housing_expenditure",
+                    "health_expenditure",
+                    "civic_expenditure",
+                    "transportation_expenditure",
+                    "partnerships_expenditure",
+                    "other_expenditure",
+                ]
+            }
+        ),
+    ] = 0
 
 
 type UIDefinition = list[SectionBlock | FieldBlock]
