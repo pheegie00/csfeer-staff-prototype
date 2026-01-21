c7b176b5101bb0656535e307fcb50c67b6c2ae89
Ryan Bagwell <ryan@ryanbagwell.com>
2026-01-21 17:06:45 -0500

    remove unused import

--- form_manager/schema/forms/base.py
+++ form_manager/schema/forms/base.py
@@ -14,7 +14,6 @@ from typing import (
 )
 
 from django import forms
-from django.forms.boundfield import BoundField
 from django.forms.renderers import TemplatesSetting
 from pydantic import BaseModel, Field, GetCoreSchemaHandler
 from pydantic_core import core_schema
