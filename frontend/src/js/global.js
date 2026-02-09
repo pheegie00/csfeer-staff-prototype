6bf65b6f44f75eef826818c7ea793d246e0a228d
Ryan Bagwell <ryan@ryanbagwell.com>
2026-02-09 16:35:21 -0500

    FE-310 - Move auto calculated field js to another file, fix some bug and add e2e tests

--- frontend/src/js/global.js
+++ frontend/src/js/global.js
@@ -2,5 +2,11 @@ import '@uswds/uswds';
 import Alpine from 'alpinejs'
 import mask from '@alpinejs/mask'
 
+// Form utilities
+import { updateCalculatedFields } from './utils/forms';
+
+// Expose form utilities globally for Alpine.js template access
+window.updateCalculatedFields = updateCalculatedFields;
+
 Alpine.plugin(mask)
 Alpine.start()
---
+++ frontend/src/js/utils/forms.js
@@ -0,0 +1,28 @@
+/**
+ * Form utility functions
+ */
+
+/**
+ * Updates calculated fields based on their source fields.
+ * This function is called when form inputs change and automatically
+ * updates any fields with the "calculated-currency-field" or "calculated-field" classes.
+ *
+ * @param {Event} e - The change event from the form
+ */
+export function updateCalculatedFields(e) {
+  const calculatedFields = e.currentTarget.querySelectorAll(".calculated-currency-field, .calculated-field");
+  calculatedFields.forEach((field) => {
+    let sourceFields = field.getAttribute("data-source-fields");
+    sourceFields = sourceFields.split(',')
+    const total = sourceFields.reduce((total, fieldName, index) => {
+      const sourceField = e.currentTarget.querySelector(`[name=${fieldName}]`)
+      if (sourceField === null) {
+        return total;
+      }
+      let numericValue = parseFloat((sourceField.value || "0.0").replace(/,/g, ''));
+      const newTotal = numericValue + total;
+      return newTotal
+    }, 0.0);
+    field.value = Intl.NumberFormat("en-US", {"style": "decimal", minimumFractionDigits: 2}).format(total);
+  })
+}
