f0eee72fc0555e1519ee3a8a766ac91be2e20413
Ryan Bagwell <ryan@ryanbagwell.com>
2026-04-01 15:05:36 -0400

    remove django_cotton_uswds app completely

--- _django_cotton_uswds/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class DjangoCottonUswdsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "django_cotton_uswds"
-    verbose_name = "Django Cotton USWDS Components"
