ad7c06145299a4ce8fd98fb9089341608280ce1d
Mohammad Taleb <240728+talebbits@users.noreply.github.com>
2026-04-14 13:04:10 -0400

    Update csfeer/backends.py
    Co-authored-by: Ryan Bagwell <ryan@ryanbagwell.com>

--- csfeer/backends.py
+++ csfeer/backends.py
@@ -73,8 +73,8 @@ class EmailOIDCAuthenticationBackend(AuthenticationBackend):
 
 
 def extend_user_with_roles(user, claims, request=None, access_token=None):
-    user.first_name = claims.get("given_name", "") or user.first_name
-    user.last_name = claims.get("family_name", "") or user.last_name
+    user.first_name = user.first_name or claims.get("given_name", "")
+    user.last_name = user.last_name or claims.get("family_name", "")
     user.save(update_fields=["first_name", "last_name"])
 
     profile, _ = UserProfile.objects.get_or_create(user=user)
