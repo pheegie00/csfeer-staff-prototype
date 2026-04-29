bbe62c3debb7e936c438ecd3d4930acae08c30ee
Mohammad Taleb <t@lebbits.com>
2026-04-29 18:13:33 -0400

    fix(fields): make workplace location field optional

--- form_manager/schema/forms/tribal_plan/fields.py
+++ form_manager/schema/forms/tribal_plan/fields.py
@@ -550,7 +550,7 @@ class TribalPlanFormFields(BaseFields):
                 title="List additional workplace location(s)",
                 description="500 characters allowed.",
                 max_length=500,
-                required=True,
+                required=False,
             )
         ],
         validators=[],
