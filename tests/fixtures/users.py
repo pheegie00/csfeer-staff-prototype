d878a87d76ee44add67a883e81ad9250395eaf31
Ryan Bagwell <ryan@ryanbagwell.com>
2026-05-01 16:33:53 -0400

    fixes

--- tests/fixtures/users.py
+++ tests/fixtures/users.py
@@ -6,12 +6,6 @@ from collections.abc import Generator
 
 import pytest
 from playwright.sync_api import Page
-from users.permissions import (
-    RECIPIENT_AUTHORIZED_OFFICIAL,
-    RECIPIENT_FORM_APPROVER,
-    RECIPIENT_FORM_EDITOR,
-    RECIPIENT_FORM_VIEWER,
-)
 
 from tests.e2e.pages.login_page import LoginPage
 from users.permissions import (
